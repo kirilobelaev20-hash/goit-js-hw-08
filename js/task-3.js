b083a81616f712e1b75e39d676ac9971e9bfd49e
kirilobelaev20-hash <_EMAIL__GITHUB>
2026-07-05 14:19:46 +0300

    =

--- js/task-3.js
+++
@@ -1,5 +0,0 @@
-const nameElement = document.querySelector('#name-input');
-nameElement.addEventListener('input', event => {
-  const nameOutput = document.querySelector('#name-output');
-  nameOutput.textContent = event.target.value.trim() === '' ? 'Anonymous' : event.target.value.trim();
-});
